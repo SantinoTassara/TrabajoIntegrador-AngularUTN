949a1bfe986e3f11c3affae3485e63b4dc001ad2
Santino Tassara Franco <santinotassarafrancoar@gmail.com>
2025-12-29 19:02:09 -0300

    Se agrega la funcionalidad de buscar chat

--- src/app/components/chats-component/chats-component.ts
+++ src/app/components/chats-component/chats-component.ts
@@ -1,17 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { ChatService } from '../../services/chat';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-chats-component',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './chats-component.html',
   styleUrl: './chats-component.css',
   standalone: true
 })
 export class ChatsComponent {
+  searchChat = signal('');
+
+  filteredChats = computed(() => {
+    const chatSearch = this.searchChat().toLowerCase();
+    const chats = this.chatService.chats();
+    if (!chatSearch) {
+      return chats;
+    }
+    return chats.filter(chat => chat.name.toLowerCase().includes(chatSearch));
+  });
+
   constructor(public chatService: ChatService, private router: Router) {
 
   }
